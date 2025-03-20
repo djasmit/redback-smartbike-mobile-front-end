c4ce7a25f4d65e0da4672bfd118570fdc336d34b
Jordan <andersonjor@deakin.edu.au>
2025-03-21 04:12:25 +1100

    update workout cards gradient

--- src/components/WorkoutCard.jsx
+++ src/components/WorkoutCard.jsx
@@ -4,19 +4,26 @@ import { LinearGradient } from "expo-linear-gradient";
 
 const WorkoutCard = ({ title, image, bgColor }) => {
   return (
-    <View
-      className="w-full h-48 my-2 rounded-[48px] overflow-hidden"
-      style={{ backgroundColor: bgColor }}
+    <LinearGradient
+      colors={[bgColor, "#95ffffff"]}
+      style={{
+        overflow: "hidden",
+        borderRadius: 48,
+        marginBottom: 4,
+        marginTop: 4,
+      }}
     >
-      <Text className="absolute top-1/3 left-4 text-black font-bold text-3xl">
-        {title}
-      </Text>
-      <Image
-        className="w-56 h-44 absolute bottom-0 right-0"
-        source={image}
-        resizeMode="contain"
-      />
-    </View>
+      <View className="w-full h-48">
+        <Text className="absolute top-1/3 left-4 text-black font-bold text-3xl">
+          {title}
+        </Text>
+        <Image
+          className="w-56 h-44 absolute bottom-0 right-0"
+          source={image}
+          resizeMode="contain"
+        />
+      </View>
+    </LinearGradient>
   );
 };
 
