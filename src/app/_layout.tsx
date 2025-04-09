08de43b1289a92d4bab5b71a5656e4b80ef062cb
Jordan <andersonjor@deakin.edu.au>
2025-04-10 02:05:22 +1000

    fix routing headers

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -6,9 +6,15 @@ const _layout = () => {
     <>
       <Stack
         screenOptions={{
-          headerShown: false,
+          headerTransparent: true,
+          headerTitle: "",
+          headerTintColor: "white",
         }}
-      />
+      >
+        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
+        <Stack.Screen name="signup" options={{ headerShown: false }} />
+        <Stack.Screen name="index" options={{ headerShown: false }} />
+      </Stack>
     </>
   );
 };
